82c69b59e75eb4caf01cb1feeb845775b16da7c4
Israel Cena <israelsjm@gmail.com>
2022-10-07 10:26:29 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p>Hello World!</p>
+    </div>
+  )
+}
+
+export default App
